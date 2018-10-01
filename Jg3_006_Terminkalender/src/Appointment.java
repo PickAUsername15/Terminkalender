65fe3e1e1be8f6551870469c5a6bdd4de8549d41
PickAUsername15 <rotdob16@new.edvhtl.at>
2018-10-01 11:11:03 +0200

    toString Appointment, add remove AppointmentModel

--- Jg3_006_Terminkalender/src/Appointment.java
+++ Jg3_006_Terminkalender/src/Appointment.java
@@ -1,3 +1,6 @@
+
+import java.time.LocalDateTime;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,5 +12,34 @@
  * @author Dominik Roth
  */
 public class Appointment {
+    private String text;
+    private LocalDateTime ldt;
+
+    public Appointment(String text, LocalDateTime ldt) {
+        this.text = text;
+        this.ldt = ldt;
+    }
+
+    public LocalDateTime getLdt() {
+        return ldt;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setLdt(LocalDateTime ldt) {
+        this.ldt = ldt;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%01d.%01d.%01d - %01d.%01d --> %s", ldt.getDayOfMonth(), ldt.getMonthValue(), ldt.getYear(), ldt.getHour(), ldt.getMinute(), text);
+    }
+    
     
 }
--- Jg3_006_Terminkalender/src/AppointmentModell.java
+++ Jg3_006_Terminkalender/src/AppointmentModell.java
@@ -1,13 +1,37 @@
+
+import java.util.ArrayList;
+import javax.swing.AbstractListModel;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author Dominik Roth
  */
-public class AppointmentModell {
+public class AppointmentModell extends AbstractListModel {
+
+    private ArrayList<Appointment> appointments = new ArrayList<>();
+
+    @Override
+    public int getSize() {
+        return appointments.size();
+    }
+
+    @Override
+    public Object getElementAt(int index) {
+        return appointments.get(index);
+    }
+    
+    void add(Appointment a){
+        appointments.add(a);
+        fireIntervalAdded(this, appointments.size()-1, appointments.size()-1);
+    }
+     void remove(Appointment a){
+        appointments.remove(a);
+        fireIntervalRemoved(this, appointments.size()-1, appointments.size()-1);
+    }
     
 }
